955811d64450bbab37efeaba336116b1bc400c85
Anilkumar-7676 <anilkumarpinniboyina143@gmail.com>
2026-03-10 18:05:49 +0530

    Update EmployeeService.java

--- src/main/java/com/kk/services/EmployeeService.java
+++ src/main/java/com/kk/services/EmployeeService.java
@@ -25,7 +25,7 @@ public class EmployeeService {
                 js.put("Name", "KK FUNDA");
                 js.put("Calling Name", "KK FUNDA");
                 js.put("DOB", "08-Nov-2011");
-                js.put("Hobbies", "Reading Technical Blogs,Teaching, Helping to Poor People..");
+                js.put("Hobbiees", "Reading Technical Blogs,Teaching, Helping to Poor People..");
                 js.put("Places he like", "His native place");
 
                 return js.toString();
